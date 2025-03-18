2e072cd00e7bf83cc1bb57bdde110379c4dcaeaf
Ben Iofel <ben@iofel.me>
2025-03-18 11:52:54 -0400

    Initial Commit

---
+++ musescore-quicklook/musescore_quicklookApp.swift
@@ -0,0 +1,17 @@
+//
+//  musescore_quicklookApp.swift
+//  musescore-quicklook
+//
+//  Created by Ben on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct musescore_quicklookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
